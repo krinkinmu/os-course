d0e2c65945049d203bcfd1c7b3fe2eece6e5d995
Mike Krinkin <krinkin.m.u@gmail.com>
2016-02-21 15:01:34 +0300

    Update memory.h for hw2
    VA/PA, va/pa - virtual|physical address conversions,
    BOOTMEM_SIZE - size of the identity mapping and the
                   mapping  right above "the hole",
    phys_t/virt_t - just typedefs

--- src/memory.h
+++ src/memory.h
@@ -13,16 +13,29 @@
 
 #define KERNEL_PHYS(x)    ((x) - KERNEL_BASE)
 #define KERNEL_VIRT(x)    ((x) + KERNEL_BASE)
+#define PA(x)             ((x) - HIGH_BASE)
+#define VA(x)             ((x) + HIGH_BASE)
 
 #ifndef __ASM_FILE__
 
 #include <stdint.h>
+
+#define BOOTMEM_SIZE      (4ull * 1024ull * 1024ull * 1024ull)
+
+typedef uintptr_t phys_t;
+typedef uintptr_t virt_t;
 
 static inline uintptr_t kernel_phys(void *addr)
 { return KERNEL_PHYS((uintptr_t)addr); }
 
 static inline void *kernel_virt(uintptr_t addr)
 { return (void *)KERNEL_VIRT(addr); }
+
+static inline phys_t pa(const void *addr)
+{ return PA((virt_t)addr); }
+
+static inline void *va(phys_t addr)
+{ return (void *)VA(addr); }
 
 #endif /*__ASM_FILE__*/
 
