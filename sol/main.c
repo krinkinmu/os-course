119bf894b60496dce1f146362e95c3fc620a594c
Mike Krinkin <krinkin.m.u@gmail.com>
2016-03-21 13:00:06 +0300

    add smoke tests for allocators

--- sol/main.c
+++ sol/main.c
@@ -1,15 +1,115 @@
+#include "kmem_cache.h"
 #include "interrupt.h"
+#include "memory.h"
 #include "serial.h"
+#include "paging.h"
 #include "stdio.h"
+#include "misc.h"
 #include "time.h"
+
+static bool range_intersect(phys_t l0, phys_t r0, phys_t l1, phys_t r1)
+{
+	if (r0 <= l1)
+		return false;
+	if (r1 <= l0)
+		return false;
+	return true;
+}
+
+static void buddy_smoke_test(void)
+{
+#define PAGES 10
+	struct page *page[PAGES];
+	int order[PAGES];
+
+	for (int i = 0; i != PAGES; ++i) {
+		page[i] = alloc_pages(i);
+		if (page[i]) {
+			const phys_t begin = page_paddr(page[i]);
+			const phys_t end = begin + (PAGE_SIZE << i);
+			printf("allocated [%#llx-%#llx]\n", begin, end - 1);
+			order[i] = i;
+		}
+	}
+
+	for (int i = 0; i != PAGES - 1; ++i) {
+		if (!page[i])
+			break;
+		for (int j = i + 1; j != PAGES; ++j) {
+			if (!page[j])
+				break;
+
+			const phys_t ibegin = page_paddr(page[i]);
+			const phys_t iend = ibegin + (PAGE_SIZE << order[i]);
+
+			const phys_t jbegin = page_paddr(page[j]);
+			const phys_t jend = jbegin + (PAGE_SIZE << order[j]);
+
+			DBG_ASSERT(!range_intersect(ibegin, iend, jbegin, jend));
+		}
+	}
+
+	for (int i = 0; i != PAGES; ++i) {
+		if (!page[i])
+			continue;
+
+		const phys_t begin = page_paddr(page[i]);
+		const phys_t end = begin + (PAGE_SIZE << i);
+		printf("freed [%#llx-%#llx]\n", begin, end - 1);
+		free_pages(page[i], order[i]);
+	}
+#undef PAGES
+}
+
+struct intlist {
+	struct list_head link;
+	int data;
+};
+
+static void slab_smoke_test(void)
+{
+#define ALLOCS 1000000
+	struct kmem_cache *cache = KMEM_CACHE(struct intlist);
+	LIST_HEAD(head);
+	int i;
+
+	for (i = 0; i != ALLOCS; ++i) {
+		struct intlist *node = kmem_cache_alloc(cache);
+
+		if (!node)
+			break;
+		node->data = i;
+		list_add_tail(&node->link, &head);
+	}
+
+	printf("Allocated %d nodes\n", i);
+
+	while (!list_empty(&head)) {
+		struct intlist *node = LIST_ENTRY(list_first(&head),
+					struct intlist, link);
+
+		list_del(&node->link);
+		kmem_cache_free(cache, node);
+	}
+
+	kmem_cache_destroy(cache);
+#undef ALLOCS
+}
 
 void main(void)
 {
 	setup_serial();
+	setup_misc();
 	setup_ints();
+	setup_memory();
+	setup_buddy();
+	setup_paging();
+	setup_alloc();
 	setup_time();
-
 	local_irq_enable();
+
+	buddy_smoke_test();
+	slab_smoke_test();
 
 	while (1);
 }
